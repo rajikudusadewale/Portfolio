556d5e7895dd0687d011353c6226b41b6991970f
linktoyinka <linktoyinka@gmail.com>
2023-03-19 21:31:31 +0100

    nav-link toggle

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ main_menu.addEventListener('click', function(){
     main_menu.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 });
+
+const nav = document.querySelector('.nav-link')
+
+nav.addEventListener('click', function(){
+    main_menu.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+})
+
+
+const nav2 = document.querySelector('.nav-link2')
+
+nav2.addEventListener('click', function(){
+    main_menu.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+})
+
+
+const nav3 = document.querySelector('.nav-link3')
+
+nav3.addEventListener('click', function(){
+    main_menu.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+})
