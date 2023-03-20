ec80ebd55634613e9e61f881c013e10a98587966
linktoyinka <linktoyinka@gmail.com>
2023-03-20 17:49:48 +0100

    updates to slider and hero btn

--- script.js
+++ script.js
@@ -28,3 +28,37 @@ nav3.addEventListener('click', function(){
     main_menu.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 })
+
+
+
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
