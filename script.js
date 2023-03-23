cc151f06969b282ec731ebcdaefca37ea6ef77a9
linktoyinka <linktoyinka@gmail.com>
2023-03-23 08:21:28 +0100

    document formatted

--- script.js
+++ script.js
@@ -1,64 +1,58 @@
-const main_menu = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.nav-mobile');
+const main_menu = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".nav-mobile");
 
-main_menu.addEventListener('click', function(){
-    main_menu.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
+main_menu.addEventListener("click", function () {
+  main_menu.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
 });
 
-const nav = document.querySelector('.nav-link')
+const nav = document.querySelector(".nav-link");
 
-nav.addEventListener('click', function(){
-    main_menu.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-})
+nav.addEventListener("click", function () {
+  main_menu.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
+});
 
+const nav2 = document.querySelector(".nav-link2");
 
-const nav2 = document.querySelector('.nav-link2')
+nav2.addEventListener("click", function () {
+  main_menu.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
+});
 
-nav2.addEventListener('click', function(){
-    main_menu.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-})
+const nav3 = document.querySelector(".nav-link3");
 
-
-const nav3 = document.querySelector('.nav-link3')
-
-nav3.addEventListener('click', function(){
-    main_menu.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-})
-
-
-
-
+nav3.addEventListener("click", function () {
+  main_menu.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
+});
 
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-  });
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
